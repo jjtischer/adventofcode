5c7922e00825166125a8a890068d1b366a286a8e
Josh Tischer <josh.tischer@aspenmesh.io>
2021-12-10 11:05:01 -0700

    AOC - comments update

--- 2021/day.sh
+++ 2021/day.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Run this script without args to setup current day, or pass in specific day 1,2 not 01, 02.
+# It will:
+#   - create a new directory for the day
+#   - download the problem input
+#   - copy the template file
+#
+
 day=$(date +'%d')
 if [ -z "$1" ];then
   mkdir $day
